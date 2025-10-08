840f4079fb3a92569f751be8c2f4dfe4ccfca7a2
rohit <50377477+caretak3r@users.noreply.github.com>
2025-10-07 19:19:53 -0700

    Update install_docker.sh

--- install_docker.sh
+++ install_docker.sh
@@ -12,3 +12,8 @@ echo \
   $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
+
+# install
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+sudo systemctl status docker
